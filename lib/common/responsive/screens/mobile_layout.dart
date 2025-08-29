07512d9dee7fba0c324a5c03cbabb5b62384a30b
rmr32 <rmr@gmx.us>
2025-08-29 11:16:37 -0500

    Mobile Layout

--- lib/common/responsive/screens/mobile_layout.dart
+++ lib/common/responsive/screens/mobile_layout.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:raspucat/utils/constants/exports.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({super.key, this.body});
@@ -7,6 +7,6 @@ class MobileLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const DesktopLayout();
   }
 }
