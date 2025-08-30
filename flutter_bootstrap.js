5538ff2d63003b1df69c07203b818ee46e5c0c58
rmr32 <rmr32@users.noreply.github.com>
2025-08-30 17:00:41 +0000

    deploy: d80e9591c5d68dee3be2862ef215ce2169c2e460

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4289065192"
+    serviceWorkerVersion: "1518138700"
   }
 });
